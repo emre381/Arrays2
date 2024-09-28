803b48ea4ad3315e781e428e8270e1553ca40646
emre381 <cihatemre381@gmail.com>
2024-09-28 11:35:40 +0300

    first commit

---
+++ Arrays2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+     class Program  
+    {
+        static void Main(string[] args)
+        {
+            int[][] numbers = { new int[] { 2, 3 }, new int[] { 4 } };
+            Console.WriteLine(numbers[1][0]);
+
+            int[][,] numbers2 = new int[3][,] { new int[,] { { 2, 3 }, { 3, 4 } },
+            new int[,]{ {4 },{5 } }, new int[,]{ { 1,2,3},{4,5,6 } }};
+            Console.WriteLine(numbers2[2][0,2]);
+            Console.ReadLine();
+
+        }
+    }
+}
